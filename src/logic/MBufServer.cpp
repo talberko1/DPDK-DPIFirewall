e0da36feba514aeac9ab20edc34940cb16977838
talberko1 <talber461@gmail.com>
2021-02-23 02:40:23 +0200

    Fixed segmentation fault by initializing after allocation

--- src/logic/MBufServer.cpp
+++ src/logic/MBufServer.cpp
@@ -7,7 +7,8 @@
 
 MBufServer::MBufServer(std::uint16_t packetBurstSize) : m_Running(false), m_MaxBurstSize(packetBurstSize) {
     std::cout << "[MBuf] Allocating " << m_MaxBurstSize * sizeof(pcpp::MBufRawPacket *) << " bytes." << std::endl;
-    m_RawPackets = (pcpp::MBufRawPacket **) ::operator new(m_MaxBurstSize * sizeof(pcpp::MBufRawPacket *));
+    m_RawPackets = static_cast<pcpp::MBufRawPacket **>(::operator new(m_MaxBurstSize * sizeof(pcpp::MBufRawPacket *)));
+    std::fill(m_RawPackets, m_RawPackets + m_MaxBurstSize, nullptr);
     if (!m_RawPackets)
         throw std::bad_alloc();
 }
